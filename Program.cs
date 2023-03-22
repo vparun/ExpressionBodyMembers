52669461a3a1d0b57a078b52689ce17d623ef3fe
Arunkumar Parthasarathy <arunkumar.ps1988@gmail.com>
2023-03-22 17:47:48 +0530

    Added program file

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ExpressionBodyMembers
+{
+    public class Money
+    {
+        private decimal value;
+
+        public decimal Value
+        {
+            get => value;
+            set => SetValue(value);
+        }
+
+        public Money(decimal value)
+        {
+            SetValue(value);
+        }
+
+        public string AsString => $"₹{value}";
+
+        public override string ToString() => $"Your money is {AsString}";
+
+
+        private void SetValue(decimal newValue)
+        {
+            value = newValue >= 0 ? newValue : throw new InvalidDataException("Value must be greater than Zero.");
+        }
+    }
+
+    public class Program
+    {
+        private static void Main()
+        {
+            var money = new Money(10);
+
+            Console.WriteLine(money.AsString);
+            Console.WriteLine(money.ToString());
+        }
+    }
+}
